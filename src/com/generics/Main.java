08b4484e92f7840bd5281f36fee3361510778930
Amit <amitlpalandurkar1998@gmail.com>
2023-07-06 19:24:28 +0530

    WellCome Page and File Structure.

---
+++ src/com/generics/Main.java
@@ -0,0 +1,7 @@
+package com.generics;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nWellCome To Find Maximum Problem using Generics project.");
+    }
+}
