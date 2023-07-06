779b9151845dc4cc1de1392ba8edc27c57211a2d
Amit <amitlpalandurkar1998@gmail.com>
2023-07-06 19:47:21 +0530

    get three values from user find max of them.

--- src/com/generics/Main.java
+++ src/com/generics/Main.java
@@ -1,7 +1,21 @@
 package com.generics;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\nWellCome To Find Maximum Problem using Generics project.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nWellCome To Find Maximum Problem using Generics project.\n");
+
+        MaxMin<Integer> maxMin = new MaxMin();
+        System.out.print("Enter The First Number (1st): ");
+        Integer firstNum = scanner.nextInt();
+        System.out.print("Enter The Second Number (2nd): ");
+        Integer secondNum = scanner.nextInt();
+        System.out.print("Enter The Third Number (3rd): ");
+        Integer thirdNum = scanner.nextInt();
+
+        int result = maxMin.checkMaxNum(firstNum,secondNum,thirdNum);
+        System.out.println("\n"+result+" is Max Number.");
     }
 }
