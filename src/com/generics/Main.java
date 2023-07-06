3cd9a0f2029c35cae44fc433eb2f6f562d90f433
Amit <amitlpalandurkar1998@gmail.com>
2023-07-06 20:27:55 +0530

    Use Sorting Method To Find Max. Number.

--- src/com/generics/Main.java
+++ src/com/generics/Main.java
@@ -4,19 +4,20 @@ import java.awt.datatransfer.StringSelection;
 import java.util.Scanner;
 
 public class Main {
-    public static <Strings> void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("\nWellCome To Find Maximum Problem using Generics project.\n");
 
-        MaxMin<String> maxMin = new MaxMin();
+        MaxMin<Integer> maxMin = new MaxMin();
         System.out.print("Enter The First Number (1st): ");
-        Strings firstNum = (Strings) scanner.nextLine();
+        Integer firstNum = scanner.nextInt();
         System.out.print("Enter The Second Number (2nd): ");
-        Strings secondNum = (Strings) scanner.nextLine();
+        Integer secondNum = scanner.nextInt();
         System.out.print("Enter The Third Number (3rd): ");
-        Strings thirdNum = (Strings) scanner.nextLine();
+        Integer thirdNum = scanner.nextInt();
 
-        Strings result = (Strings) maxMin.checkMaxNum((String) firstNum, (String) secondNum, (String) thirdNum);
+        Integer result = maxMin.checkMaxNum( firstNum, secondNum, thirdNum);
         System.out.println("\n"+result+" is Max Number.");
+
     }
 }
--- src/com/generics/MaxMin.java
+++ src/com/generics/MaxMin.java
@@ -1,18 +1,41 @@
 package com.generics;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class MaxMin <I extends Comparable<I>>{
 
     I max ;
+//    I checkMaxNum(I firstNum, I secondNum, I thirdNum) {
+//        max=firstNum;
+//        if (secondNum.compareTo(max) > 0){
+//            max=secondNum;
+//        }
+//        if (thirdNum.compareTo(max) > 0){
+//            max=thirdNum;
+//        }
+//        return max;
+//    }
+//    I checkMaxNum(I firstNum, I secondNum, I thirdNum) {
+//        I [] array = new <I> [3];
+//        I firstNum1 = firstNum;
+//        array[0]= firstNum1;
+//        array[1]=secondNum;
+//        array[2]=thirdNum;
+//        List<I> Array;
+//        max = Array.sort(array<I>);
+//        result max;
+//    }
     I checkMaxNum(I firstNum, I secondNum, I thirdNum) {
-        max=firstNum;
-        if (secondNum.compareTo(max) > 0){
-            max=secondNum;
-        }
-        if (thirdNum.compareTo(max) > 0){
-            max=thirdNum;
-        }
+        Integer[] array = new Integer[3];
+        array[0] = (Integer) firstNum;
+        array[1] = (Integer) secondNum;
+        array[2] = (Integer) thirdNum;
+
+        Arrays.sort(array);
+
+        I max = (I) array[array.length - 1];
         return max;
     }
 
